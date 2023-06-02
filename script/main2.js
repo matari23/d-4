bbc4072c744eed6e4bdca076763c0aad4585524e
matari23 <matari23@naver.com>
2023-06-02 17:39:31 +0900

    sub메뉴 다른버전

---
+++ script/main2.js
@@ -0,0 +1,37 @@
+$(function(){
+    //메뉴
+    $(".main>li, .sub").hover(function(){
+    $(".sub").stop().show();
+    },function(){
+    $(".sub").stop().hide();
+    })
+    $(".sub >li").hover(function(){
+        var n= $(this).index();
+        console.log("n:",n)
+        $(".main>li").eq(n).find("a").addClass("on")
+    },function(){
+        $(".main>li>a").removeClass("on")
+    })
+    
+    //이미지슬라이드
+    $(".move li:gt(0)").hide();
+    var n=0;
+    setInterval(function(){
+    $(".move li").eq(n).fadeOut();
+    if(n==2){
+        n=0;
+    }else{
+        n++;
+    }
+    $(".move li").eq(n).fadeIn();
+    },3000)
+    
+    
+    //팝업
+    $(".pop").click(function(){
+        $(".popup").show();
+    })
+    $(".close").click(function(){
+        $(".popup").hide();
+    })
+    })//jq
